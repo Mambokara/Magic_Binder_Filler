a675e03b38a1e0102705d520328a23f731a3342e
Mambokara <corentin.bergaentzle@epitech.eu>
2024-09-01 22:21:28 +0200

    [+] Finished the comparison and the adding of the new values into the binder csv file. Also remove the acrds put into the binder from the box to still have the same count of cards.

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ class Cards {
             string tmp_infos;
             fstream file(link);
             vector<string> Card;
-            
+
             getline(file, tmp_infos, '\r');
             while (getline(file, tmp_infos, '\r')) {
                 Card.push_back(tmp_infos);
@@ -35,7 +35,7 @@ class Cards {
             file.close();
             return Card;
         }
-        
+
         vector<string> separate_infos(string info){
             vector<string> tmp;
             string tmp_string;
@@ -48,32 +48,48 @@ class Cards {
                 }
                 tmp_string.push_back(info[i]);
             }
+            tmp.push_back(tmp_string);
             return tmp;
         }
 };
 
-vector<vector<string>> compare_infos(vector<vector<string>> box, vector<vector<string>> binder) {
+vector<vector<vector<string>>> compare_infos(vector<vector<string>> box, vector<vector<string>> binder) {
+    vector<vector<vector<string>>> storage;
     int found = 0;
+    string str;
+    int tmp = 0;
     for (int i = 0; i < box.size() - 1; i++) {
         for (int j = 0; j < binder.size(); j++) {
-            if (box[i][2] == binder[j][2]) {
+            if (box[i][2].compare(binder[j][2]) == 0) {
                 found = 1;
                 break;
             }
         }
         if (found != 1) {
-            box[i][0][2] = '1';
-            box[i][1][1] = '1';
             binder.push_back(box[i]);
+            binder.back()[0][2] = '1';
+            binder.back()[1][1] = '1';
+            str = box[i][0].substr(2, box[i][0].size() - 3);
+            tmp = stoi(str);
+            tmp--;
+            if (tmp <= 0) {
+                box.erase(box.begin() + i);
+                i--;
+            } else {
+                box[i][0] = "\n\"" + to_string(tmp) + "\"";
+                box[i][1] = "\"" + to_string(tmp) + "\"";
+            }
         } else {
             found = 0;
         }
     }
-    return binder;
+    storage.push_back(binder);
+    storage.push_back(box);
+    return storage;
 }
 
-void print_in_file(vector<vector<string>> new_csv) {
-    ofstream output_file("Binder.csv");
+void print_in_file(vector<vector<string>> new_csv, string file_name) {
+    ofstream output_file(file_name);
 
     output_file << "\"Count\",\"Tradelist Count\",\"Name\",\"Edition\",\"Condition\",\"Language\",\"Foil\",\"Tags\",\"Last Modified\",\"Collector Number\",\"Alter\",\"Proxy\",\"Purchase Price\"\r";
     for (int i = 0; i < new_csv.size(); i++) {
@@ -91,6 +107,8 @@ void print_in_file(vector<vector<string>> new_csv) {
 int main() {
     Cards Box_csv("moxfield_haves_2024-08-29-1530Z.csv");
     Cards Binder_csv("empty.csv");
-    vector<vector<string>> new_csv = compare_infos(Box_csv.cards_info, Binder_csv.cards_info);
-    print_in_file(new_csv);
+    vector<vector<vector<string>>> new_csv = compare_infos(Box_csv.cards_info, Binder_csv.cards_info);
+    print_in_file(new_csv[0], "Binder.csv");
+    print_in_file(new_csv[1], "test.csv");
+
 }
